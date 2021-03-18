b167180a438121f2641e6e8dc902382f2571ad50
Suho Kwon <34533493+Knix008@users.noreply.github.com>
2021-03-19 08:49:09 +0900

    Add some example cpp and py files.
    Add some example files.

---
+++ Examples/testFieldElement.py
@@ -0,0 +1,8 @@
+# Example
+import Field as fe
+
+a = fe.FieldElement(7, 13)
+b = fe.FieldElement(6, 13)
+
+print(a == b)
+print(a == a)
---
+++ Examples/testRandom.py
@@ -0,0 +1,7 @@
+import os
+
+result = str(os.urandom(10))
+print(type(result))
+
+print(type(os.urandom(10)))
+
